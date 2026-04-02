4cee0885dddd7744880cf0cac78b3075b6996864
Ilkersarica <ilker@thekompanion.com>
2026-04-02 23:44:03 +0300

    fix: api lol

--- src/services/api.ts
+++ src/services/api.ts
@@ -8,7 +8,7 @@ const DEV_ENDPOINT = Platform.select({
   ios: "http://localhost:3000/api",
 });
 
-export const ENDPOINT = DEV_ENDPOINT;
+export const ENDPOINT = LIVE_ENDPOINT;
 
 const api = axios.create({
   baseURL: ENDPOINT,
