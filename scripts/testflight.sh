72672d6eb04cf687277a558e864acbbe688d2143
Ilkersarica <ilker@thekompanion.com>
2026-04-22 01:05:56 +0300

    feat: script changes

--- scripts/testflight.sh
+++ scripts/testflight.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-if [ -z "$1" ] || [ -z "$2" ]; then
-  echo "Kullanım: yarn testflight <version> <build>"
-  echo "Örnek:    yarn testflight 1.1.2 42"
-  exit 1
+if [ -n "$1" ]; then
+  cd ios && bundle exec fastlane beta version:"$1"
+else
+  cd ios && bundle exec fastlane beta
 fi
-
-cd ios && bundle exec fastlane beta version:$1 build:$2
